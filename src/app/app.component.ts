ae04b59f956852e10f5e3b0f1b8c1b5cfc250098
AbhilashRocks <abhilash.tatikonda@focalcxm.com>
2020-06-12 19:41:46 +0530

    icons added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faCoffee,
+  faBars,
+  faTimes,
+  faEdit,
+  faSort,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'IEsupport';
   faCoffee = faCoffee;
+  faBars = faBars;
+  faTimes = faTimes;
+  faEdit = faEdit;
+  faSort = faSort;
 }
